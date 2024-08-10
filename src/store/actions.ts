3feda77f160c62661df4de99c34ea8dd5776738d
Jeremiah Lena <jeremiahlena13@gmail.com>
2024-08-11 00:28:42 +0100

    trying to convert next to vite

--- src/store/actions.ts
+++
@@ -1,4 +0,0 @@
-export * from './slices/collections-slice'
-export * from './slices/folder-slice'
-export * from './slices/request-tab-slice'
-export * from './slices/requests-slice'
--- src/store/slices/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as collectionsReducer } from './collections-slice'
-export { default as foldersReducer } from './folder-slice'
-export { default as requestTabsReducer } from './request-tab-slice'
-export { default as requestsReducer } from './requests-slice'
